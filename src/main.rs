f432798f923ba94f62e22d01b8447ca1f38dee97
MuGee <61751306+Mu-Gee@users.noreply.github.com>
2024-07-01 16:52:04 +0300

    Editing form data handling and submission

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,30 @@ mod qr;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use actix_files::Files;
 use qr::qrgen;
+use serde::Deserialize;
+use serde_json::json;
 
-/*#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}*/
+#[derive(Deserialize, Debug)]
+struct FormData {
+    name: String,
+    phone: String,
+    email: String,
+}
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
+#[post("/submit-details")]
+async fn submit_details(form: web::Json<FormData>) -> impl Responder {
+    println!("Received form data: {:?}", form);
+    // Will process form data later
+    let response = format!(
+        "Received details: Name: {}, Phone: {}, Email: {}",
+        form.name, form.phone, form.email
+    );
+    HttpResponse::Ok().json(json!({"status": "success", "message": response}))
+}
+
+#[get("/submit-details")]
+async fn submit_details_get() -> impl Responder {
+    HttpResponse::Ok().body("Nice try this endpoint is for form submissions only.")
 }
 
 
@@ -28,9 +43,9 @@ async fn manual_hello() -> impl Responder {
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            //.service(hello)
-            .service(echo)
             .service(generate_qr)
+            .service(submit_details)
+            .service(submit_details_get)
             .route("/hey", web::get().to(manual_hello))
             .service(Files::new("/", "./FrontEnd").index_file("index.html"))
     })
